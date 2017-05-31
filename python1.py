1ab47c980a30c57e6911f76b56ef7db35469fb1b
david <davideltonhall@gmail.com>
2017-05-30 17:02:59 -0700

    code cleanup

--- python1.py
+++ python1.py
@@ -1,37 +1,27 @@
 import turtle
 
-def draw_shapes():
-  window = turtle.Screen()
-  window.bgcolor("red")
-  brad = turtle.Turtle()
-  brad.speed("fastest")
-
-  for j in range(0,100):
-    for i in range (0,4):
-      brad.forward(100)
-      brad.right(90)
-    brad.right(2)
-  
 def draw_triangle(brad, length, depth):
   brad.setheading(300)
   for i in range(0,3):    
     brad.forward(length)
     brad.right(120)
 
-  if depth > 0:
-    draw_triangle(brad, length / 2, depth - 1)
-    brad.setheading(300)
-    brad.forward(length / 2)
-    draw_triangle(brad, length / 2, depth - 1)
-    brad.setheading(300)
-    brad.forward(length / 2)
-    brad.right(120)
-    brad.forward(length)
-    brad.right(120)
-    brad.forward(length / 2)
-    draw_triangle(brad, length / 2, depth - 1)
-    brad.setheading(60)
-    brad.forward(length / 2)
+  if depth == 0:
+    return
+  
+  draw_triangle(brad, length / 2, depth - 1)
+  brad.setheading(300)
+  brad.forward(length / 2)
+  draw_triangle(brad, length / 2, depth - 1)
+  brad.setheading(300)
+  brad.forward(length / 2)
+  brad.right(120)
+  brad.forward(length)
+  brad.right(120)
+  brad.forward(length / 2)
+  draw_triangle(brad, length / 2, depth - 1)
+  brad.setheading(60)
+  brad.forward(length / 2)
 
 def draw():
   window = turtle.Screen()
@@ -39,18 +29,6 @@ def draw():
   brad = turtle.Turtle()
 
   draw_triangle(brad, 200, 3)
-
-  # draw_triangle(brad, 150)
-  # brad.forward(150)
-  # draw_triangle(brad, 150)
-  # brad.forward(150)
-  # brad.right(120)
-  # brad.forward(300)
-  # brad.right(120)
-  # brad.forward(150)
-  # draw_triangle(brad,150)
-  # brad.setheading(60)
-  # brad.forward(150)
 
   window.exitonclick()
 
